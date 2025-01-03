68b579d173a9eef2a781f6bfba942606f0091c8a
Kabir08 <62639358+Kabir08@users.noreply.github.com>
2025-01-04 00:13:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
 import streamlit as st
 import pickle
-import os
-import requests
 import helper
 
-# Function to download the model file from Google Drive
-def download_model(file_url, output_path):
-    if not os.path.exists(output_path):
-        st.text("Downloading model file...")
-        response = requests.get(file_url, stream=True)
-        with open(output_path, "wb") as f:
-            for chunk in response.iter_content(chunk_size=8192):
-                if chunk:
-                    f.write(chunk)
-        st.text("Download complete!")
-
-# Link to the hosted model.pkl file on Google Drive
-MODEL_URL = "https://drive.google.com/uc?export=download&id=1OWbj2U5wJKUzP6UEyC7HW_Ji-Gw7SXxj"
+# Load the model directly from the root directory
 MODEL_PATH = "model.pkl"
-
-# Download the model if not present
-download_model(MODEL_URL, MODEL_PATH)
 
 # Load the model
 model = pickle.load(open(MODEL_PATH, 'rb'))
